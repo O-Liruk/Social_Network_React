15b73c74ddbab147c011b74df186e7094f60b01d
Oleksandr <sasha.liruk@gmail.com>
2021-04-16 11:46:17 +0300

    First_progect_42.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/NavBar.jsx';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/redux_store";
+import store from "./redux/redux-store";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
