65260e291027a0fcb5122f2faa55a7657d34e24e
Oleksandr <sasha.liruk@gmail.com>
2021-04-23 17:09:59 +0300

    first_progect_55_pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,26 +1,35 @@
-import React from "react";
-import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/users-reducer ";
-import Users from "./Users";
+import React from 'react';
+import Users from "./Users.jsx";
+import {connect} from "react-redux";
+import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../redux/users-reducer.js";
 
 let mapStateToProps = (state) => {
-    return{
-        users: state.usersPage.users
-    }
-}
-let mapDispatchToProps = (dispatch)=>{
     return {
-        follow:(userId)=> {
-            dispatch(followAC(userId))
-        },
-        unfollow:(userId)=> {
-            dispatch(unfollowAC(userId))
-        }, 
-        setUsers:(users)=> {
-            dispatch(setUsersAC(users))
-        }, 
-
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (Users);
+let mapDispatchToProps = (dispatch) => {
+    return {
+        follow: (userId) => {
+            dispatch(followAC(userId));
+        },
+        unfollow: (userId) => {
+            dispatch(unfollowAC(userId));
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users));
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setUsersTotalCountAC(totalCount))
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
