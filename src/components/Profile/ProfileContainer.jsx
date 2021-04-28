65cec60c2ef01415e2caa22c354ddc11c6f4e90b
Oleksandr <sasha.liruk@gmail.com>
2021-04-29 00:05:26 +0300

    first_progect_60_withRouter

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 import React from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router";
 import { setUsersProfile } from "../../redux/profile-reducer";
 import Profile from "./Profile";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {userId=2}
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUsersProfile(response.data);
       });
@@ -19,4 +22,7 @@ class ProfileContainer extends React.Component {
 let mapStateToProps = (state) => ({
  profile: state.profilePage.profile
 })
-export default connect(mapStateToProps,{ setUsersProfile } )(ProfileContainer);
+
+ let WithUrlDataContainerComponent =  withRouter(ProfileContainer);
+
+export default connect(mapStateToProps,{ setUsersProfile } )(WithUrlDataContainerComponent);
