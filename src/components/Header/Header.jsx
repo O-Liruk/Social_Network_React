48c4ef5a052def6c355827f237504f9b3fca94db
Oleksandr <sasha.liruk@gmail.com>
2021-04-30 19:08:05 +0300

    first_progect_61_auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import s from './Header.module.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./Header.module.css";
 
-const Header = () => {
-    return <header className={s.header}>
-        <img src='https://cdn.logo.com/hotlink-ok/logo-social.png' />
+const Header = (props) => {
+  return (
+    <header className={s.header}>
+      <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" />
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
     </header>
-}
+  );
+};
 
 export default Header;
