a5d46428dbb52590c14ae4cc09d59f5dbc0df63a
Oleksandr <sasha.liruk@gmail.com>
2021-05-25 17:04:58 +0300

    first_progect_95_github_pages

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ let AppContainer = compose(
 
 const SamuraiJsApp = (props) => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename = {process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer />
       </Provider>
